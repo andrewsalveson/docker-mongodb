24ae390d4cedfc5d7a686396b236083535a79c96
Andrew Salveson <andrew.salveson@perkinswill.com>
2016-01-26 10:03:48 -0500

    updated script

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 # docker-mongodb
 sudo mkdir /data/mongodb &&  /
-  sudo docker build -t  $1/mongodb . && /
+  sudo docker pull mongo && /
+  sudo docker build mongo && /
   sudo docker run -v /data/mongodb --name mongo -d mongo mongod --smallfiles
