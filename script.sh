d20fe13e65ae36f98f19bf5d83463fea46e1ca5c
Andrew Salveson <andrew.salveson@perkinswill.com>
2016-01-27 12:35:05 -0500

    linebreak fix

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 # docker-mongodb
-sudo mkdir /data/mongodb &&  /
-  sudo docker pull mongo && /
-  sudo docker build mongo && /
+sudo mkdir /data/mongodb &&  \
+  sudo docker pull mongo && \
+  sudo docker build mongo && \
   sudo docker run -v /data/mongodb --name mongo -d mongo mongod --smallfiles
