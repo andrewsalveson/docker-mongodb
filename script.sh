6d34be5dd916c287913ad8b488fb27983a7aace7
Codeio Rhymes With Rodeo <codeio@perkinswill.com>
2016-07-05 15:25:26 -0400

    updated volume link

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 sudo mkdir /data/mongodb &&  \
   sudo docker pull mongo && \
   sudo docker build mongo && \
-  sudo docker run -v /data/mongodb --name mongo -d mongo mongod --smallfiles
+  sudo docker run -v /data/mongodb:/data/db --name mongo -d mongo mongod --smallfiles
