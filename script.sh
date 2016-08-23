ff19a68c465f092a48309dab925a8d8cdff703ae
Andrew Salveson <andrewsalveson@gmail.com>
2016-08-23 16:12:11 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,6 @@
 sudo mkdir -p /data/mongodb &&  \
   sudo docker pull mongo && \
   sudo docker build mongo && \
-  sudo docker rm mongo && \
+  sudo docker stop mongo && \ # will throw a warning if not exists, but that's okay
+  sudo docker rm mongo && \ # ditto
   sudo docker run -v /data/mongodb:/data/db --name mongo -d mongo mongod --smallfiles
