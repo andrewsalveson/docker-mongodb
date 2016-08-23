fb90cdb75fdfd76e3b3431deeec8266b52d15d46
Andrew Salveson <andrewsalveson@gmail.com>
2016-08-23 16:11:09 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 # docker-mongodb
-sudo mkdir /data/mongodb &&  \
+sudo mkdir -p /data/mongodb &&  \
   sudo docker pull mongo && \
   sudo docker build mongo && \
+  sudo docker rm mongo && \
   sudo docker run -v /data/mongodb:/data/db --name mongo -d mongo mongod --smallfiles
